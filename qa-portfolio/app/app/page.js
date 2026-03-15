80f1570c78d630e9a8bbf30a88ea1ba8d1218c3c
Faizan-189 <faizannaeem095@gmail.com>
2026-03-15 22:19:44 +0500

    Create page.js

---
+++ qa-portfolio/app/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Faizan QA Portfolio</h1>
+      <p>Welcome to my QA testing portfolio.</p>
+    </main>
+  );
+}
